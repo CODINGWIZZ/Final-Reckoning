dee3501735c86dec8311c914b5d3befff57abf22
Antioxidanter <119657988+Antioxidanter@users.noreply.github.com>
2022-12-19 12:58:54 +0100

    test för UI och TMPro

--- Final Reckoning/Assets/Scripts/Inventory/Inventory scripts/MouseItemData.cs
+++ Final Reckoning/Assets/Scripts/Inventory/Inventory scripts/MouseItemData.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro.Examples;
+using TMPro;
+using UnityEngine.UIElements;
+
 
 public class MouseItemData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Image ItemSprite;
+    public TextMeshProFloatingText TextMeshProFloatingText;
 }
